64d37566114e1ff9a9c4c38d8961bcf7f8fedb5f
Sulthan Laksono Ramadhan <1911500641@student.budiluhur.ac.id>
2023-12-21 16:40:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composearticle.ui.theme.ComposeArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeArticleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeArticle()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageArticle() {
+    Image(
+        painter = painterResource(id = R.drawable.bg_compose_background),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun TitleArticle(title: String, modifier: Modifier = Modifier) {
+    Text(
+        text = title,
+        fontSize = 24.sp,
+        modifier = modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun ContentArticle(modifier: Modifier = Modifier){
+    Column {
+        Text(
+            text = stringResource(R.string.article_paragraph_1),
+            modifier = modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.article_paragraph_2),
+            modifier = modifier.padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun HomeArticle(){
+    Column {
+        ImageArticle()
+        TitleArticle(title = stringResource(id = R.string.article_title))
+        ContentArticle()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeArticleTheme {
+        HomeArticle()
+    }
+}
